5ae25d45f6da3b334cb3e99932ea14d84f4e12ef
David Haynes (C) <David.Haynes@ordnancesurvey.co.uk>
2014-12-10 11:18:36 +0000

    Initial commit. Framework project creation and .gitignore.

---
+++ OSGridPointConversion/OSGridPointConversion.h
@@ -0,0 +1,19 @@
+//
+//  OSGridPointConversion.h
+//  OSGridPointConversion
+//
+//  Created by David Haynes (C) on 10/12/2014.
+//  Copyright (c) 2014 Ordnance Survey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OSGridPointConversion.
+FOUNDATION_EXPORT double OSGridPointConversionVersionNumber;
+
+//! Project version string for OSGridPointConversion.
+FOUNDATION_EXPORT const unsigned char OSGridPointConversionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OSGridPointConversion/PublicHeader.h>
+
+
---
+++ OSGridPointConversionTests/OSGridPointConversionTests.m
@@ -0,0 +1,40 @@
+//
+//  OSGridPointConversionTests.m
+//  OSGridPointConversionTests
+//
+//  Created by David Haynes (C) on 10/12/2014.
+//  Copyright (c) 2014 Ordnance Survey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface OSGridPointConversionTests : XCTestCase
+
+@end
+
+@implementation OSGridPointConversionTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
