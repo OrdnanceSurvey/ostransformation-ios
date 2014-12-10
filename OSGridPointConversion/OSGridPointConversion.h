5da9c1547c18631ed1a0e45f1c2bf6c365a4061b
David Haynes (C) <David.Haynes@ordnancesurvey.co.uk>
2014-12-10 15:36:53 +0000

    Add universal proj4 library. Add tests from OSMap converted to XCTest. Include OSGridPoint.h in framework header.

--- OSGridPointConversion/OSGridPointConversion.h
+++ OSGridPointConversion/OSGridPointConversion.h
@@ -15,5 +15,5 @@ FOUNDATION_EXPORT double OSGridPointConversionVersionNumber;
 FOUNDATION_EXPORT const unsigned char OSGridPointConversionVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <OSGridPointConversion/PublicHeader.h>
-
+#import <OSGridPointConversion/OSGridPoint.h>
 
