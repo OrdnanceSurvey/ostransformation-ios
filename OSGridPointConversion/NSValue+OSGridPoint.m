9ae268ac6f39ca18724d807f2c42f30c64cc3953
Shri <Shikantreddy.Tekale@ordnancesurvey.co.uk>
2015-01-15 16:37:19 +0000

    Added category on NSValue

---
+++ OSGridPointConversion/NSValue+OSGridPoint.m
@@ -0,0 +1,23 @@
+//
+//  NSValue+OSGridPoint.m
+//  OSGridPointConversion
+//
+//  Created by Shrikantreddy Tekale on 15/01/2015.
+//  Copyright (c) 2015 Ordnance Survey. All rights reserved.
+//
+
+#import "NSValue+OSGridPoint.h"
+
+@implementation NSValue (OSGridPoint)
+
++ (instancetype)valueWithGridPoint:(OSGridPoint)gridPoint {
+    return [NSValue value:&gridPoint withObjCType:@encode(OSGridPoint)];
+}
+
+- (OSGridPoint)gridPointValue {
+    OSGridPoint point;
+    [self getValue:&point];
+    return point;
+}
+
+@end
