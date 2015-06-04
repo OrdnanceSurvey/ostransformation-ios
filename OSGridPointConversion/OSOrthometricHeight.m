3847451e8987bea3d849ecfd74eebfc8be0530ec
Dave Hardiman <dave.hardiman@ordnancesurvey.co.uk>
2015-06-04 12:26:16 +0100

    MMI-506 Add tests and correct cell size error

--- OSGridPointConversion/OSOrthometricHeight.m
+++ OSGridPointConversion/OSOrthometricHeight.m
@@ -33,24 +33,26 @@ _OSGM02Record _OSGM02RecordForPoint(OSRMProjectedPoint point) {
     return _OSGM02RecordAtIndex(recordIndex);
 }
 
+const double OSGM02CellSize = 1000;
+
 CLLocationDistance OSOrthomtricHeightForLocation(CLLocation *location) {
     OSRMProjectedRect theBounds = OSRMProjectedRectMake(-180.0, -90, 360.0, 180.0);
-    OSRMProjection *proj = [[OSRMProjection alloc] initWithString:@"+proj=tmerc +lat_0=49 +lon_0=-2 +k=0.9996012717 +x_0=400000 +y_0=-100000 +ellps=GRS80" inBounds:theBounds];
+    OSRMProjection *proj = [[OSRMProjection alloc] initWithString:@"+proj=tmerc +lat_0=49 +lon_0=-2 +k=0.9996012717 +x_0=400000 +y_0=-100000 +ellps=WGS84" inBounds:theBounds];
     OSRMProjectedPoint point = [proj coordinateToProjectedPoint:location.coordinate];
-    NSInteger eastIndex0 = (int)(point.x / 1000);
-    NSInteger northIndex0 = (int)(point.y / 1000);
-    double x0 = eastIndex0 * 1000;
-    double y0 = northIndex0 * 1000;
+    NSInteger eastIndex0 = (int)(point.x / OSGM02CellSize);
+    NSInteger northIndex0 = (int)(point.y / OSGM02CellSize);
+    double x0 = eastIndex0 * OSGM02CellSize;
+    double y0 = northIndex0 * OSGM02CellSize;
     OSRMProjectedPoint point0 = OSRMProjectedPointMake(x0, y0);
     _OSGM02Record record0 = _OSGM02RecordForPoint(point0);
-    _OSGM02Record record1 = _OSGM02RecordForPoint(OSRMProjectedPointMake(point.x + 1, point.y));
-    _OSGM02Record record2 = _OSGM02RecordForPoint(OSRMProjectedPointMake(point.x + 1, point.y + 1));
-    _OSGM02Record record3 = _OSGM02RecordForPoint(OSRMProjectedPointMake(point.x, point.y + 1));
+    _OSGM02Record record1 = _OSGM02RecordForPoint(OSRMProjectedPointMake(point.x + OSGM02CellSize, point.y));
+    _OSGM02Record record2 = _OSGM02RecordForPoint(OSRMProjectedPointMake(point.x + OSGM02CellSize, point.y + OSGM02CellSize));
+    _OSGM02Record record3 = _OSGM02RecordForPoint(OSRMProjectedPointMake(point.x, point.y + OSGM02CellSize));
 
     double dx = point.x - x0;
     double dy = point.y - y0;
-    double t = dx / 1000;
-    double u = dy / 1000;
+    double t = dx / OSGM02CellSize;
+    double u = dy / OSGM02CellSize;
     double shiftGeoid = ((1 - t) * (1 - u) * record0.geoidUndulation) + (t * (1 - u) * record1.geoidUndulation) + (t * u * record2.geoidUndulation) + ((1 - t) * u * record3.geoidUndulation);
     return location.altitude - shiftGeoid;
 }
---
+++ OSGridPointConversionTests/OSOrthometricHeightTests.m
@@ -0,0 +1,105 @@
+//
+//  OSOrthometricHeightTests.m
+//  OSGridPointConversion
+//
+//  Created by Dave Hardiman on 04/06/2015.
+//  Copyright (c) 2015 Ordnance Survey. All rights reserved.
+//
+
+@import XCTest;
+@import CoreLocation;
+@import OSGridPointConversion;
+
+@interface OSOrthometricHeightTestCase : NSObject
+@property (nonatomic, copy) NSString *stationName;
+@property (nonatomic, strong) CLLocation *inputLocation;
+@property (nonatomic, assign) CLLocationDistance expectedHeight;
+
++ (instancetype)testCaseWithStationName:(NSString *)stationName
+                                  latNS:(NSString *)latNS
+                                 latDeg:(NSInteger)latDeg
+                                 latMin:(NSInteger)latMin
+                                 latSec:(double)latSec
+                                  lonEW:(NSString *)lonEW
+                                 lonDeg:(NSInteger)lonDeg
+                                 lonMin:(NSInteger)lonMin
+                                 lonSec:(double)lonSec
+                           etrs89Height:(double)etrs89Height
+                              odnHeight:(double)odnHeight;
+
+@end
+
+@interface OSOrthometricHeightTests : XCTestCase
+@property (nonatomic, strong) NSArray *testData;
+@end
+
+@implementation OSOrthometricHeightTests
+
+- (NSArray *)testData {
+    if (!_testData) {
+        NSMutableArray *testData = NSMutableArray.array;
+
+        NSString *testCSV = [NSString stringWithContentsOfURL:[[NSBundle bundleForClass:self.class] URLForResource:@"OSTN02_OSGM02Tests_Out" withExtension:@"csv"] encoding:NSUTF8StringEncoding error:nil];
+        NSArray *lines = [testCSV componentsSeparatedByString:@"\r\n"];
+        [lines enumerateObjectsUsingBlock:^(NSString *line, NSUInteger idx, BOOL *stop) {
+            if (idx == 0 || idx == lines.count - 1) {
+                return;
+            }
+            NSArray *columns = [line componentsSeparatedByString:@","];
+            if (columns.count < 27) {
+                // TODO: Handle tests outside polygon
+                return;
+            }
+            OSOrthometricHeightTestCase *testCase = [OSOrthometricHeightTestCase testCaseWithStationName:columns[0] latNS:columns[4] latDeg:[columns[5] integerValue] latMin:[columns[6] integerValue] latSec:[columns[7] doubleValue] lonEW:columns[8] lonDeg:[columns[9] integerValue] lonMin:[columns[10] integerValue] lonSec:[columns[11] doubleValue] etrs89Height:[columns[12] doubleValue] odnHeight:[columns[25] doubleValue]];
+            [testData addObject:testCase];
+        }];
+
+        _testData = testData.copy;
+    }
+    return _testData;
+}
+
+- (void)testConversionFromEtrs89HeightToOrthometricHeight {
+    [self.testData enumerateObjectsUsingBlock:^(OSOrthometricHeightTestCase *testCase, NSUInteger idx, BOOL *stop) {
+        CLLocationDistance orthometricHeight = OSOrthomtricHeightForLocation(testCase.inputLocation);
+        XCTAssertEqualWithAccuracy(orthometricHeight, testCase.expectedHeight, 0.001, @"%@", testCase.stationName);
+    }];
+}
+
+@end
+
+@implementation OSOrthometricHeightTestCase
+
++ (instancetype)testCaseWithStationName:(NSString *)stationName latNS:(NSString *)latNS latDeg:(NSInteger)latDeg latMin:(NSInteger)latMin latSec:(double)latSec lonEW:(NSString *)lonEW lonDeg:(NSInteger)lonDeg lonMin:(NSInteger)lonMin lonSec:(double)lonSec etrs89Height:(double)etrs89Height odnHeight:(double)odnHeight {
+    OSOrthometricHeightTestCase *testCase = [[OSOrthometricHeightTestCase alloc] init];
+    testCase.stationName = stationName;
+
+    CLLocationDegrees lat = [self degreesForLatNS:latNS latDeg:latDeg latMin:latMin latSec:latSec];
+    CLLocationDegrees lon = [self degreesForLonEW:lonEW lonDeg:lonDeg lonMin:lonMin lonSec:lonSec];
+
+    testCase.inputLocation = [[CLLocation alloc] initWithCoordinate:CLLocationCoordinate2DMake(lat, lon) altitude:etrs89Height horizontalAccuracy:0.0 verticalAccuracy:0.0 timestamp:NSDate.date];
+    testCase.expectedHeight = odnHeight;
+    return testCase;
+}
+
++ (CLLocationDegrees)degreesForLatNS:(NSString *)latNS latDeg:(NSInteger)latDeg latMin:(NSInteger)latMin latSec:(double)latSec {
+    CLLocationDegrees degrees = [self degreesForDeg:latDeg min:latMin sec:latSec];
+    return [latNS isEqualToString:@"S"] ? -1 * degrees : degrees;
+}
+
++ (CLLocationDegrees)degreesForLonEW:(NSString *)lonEW lonDeg:(NSInteger)lonDeg lonMin:(NSInteger)lonMin lonSec:(double)lonSec {
+    CLLocationDegrees degrees = [self degreesForDeg:lonDeg min:lonMin sec:lonSec];
+    return [lonEW isEqualToString:@"W"] ? -1 * degrees : degrees;
+}
+
+double const OSDMSConverterMinutesPerDegree = 60.0;
+double const OSDMSConverterSecondsPerMinute = 60.0;
+double const OSDMSConverterSecondsPerDegree = 3600.0;
+
++ (CLLocationDegrees)degreesForDeg:(NSInteger)degrees min:(NSInteger)minutes sec:(double)seconds {
+    double result = (degrees * OSDMSConverterSecondsPerDegree) + (minutes * OSDMSConverterMinutesPerDegree) + seconds;
+    result /= OSDMSConverterSecondsPerDegree;
+    return result;
+}
+
+@end
